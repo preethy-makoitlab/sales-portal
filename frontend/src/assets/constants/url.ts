63c7a25f7bf630a67d2c9f752a042481d6eb3591
Preethy Srikanthan <srikpreethy@gmail.com>
2023-03-09 06:40:24 +0000

    app setup

---
+++ frontend/src/assets/constants/url.ts
@@ -0,0 +1,15 @@
+import { InjectionToken } from '@angular/core';
+import { environment } from 'src/environments/environment.dev';
+export class ApiConstants {
+ public static url = {
+  anyo:{
+    serviceUrl:environment.serviceUrl,
+    serviceUrl2:environment.serviceUrl2,
+    serviceUrl3:environment.serviceUrl3,
+auth:{
+
+    }
+  }
+
+}
+}
