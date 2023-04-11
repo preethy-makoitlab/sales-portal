20fe8203a3f27b214a1765f62015a88fa1823e17
dev-saravana <dev@veract>
2023-04-11 10:47:17 +0530

    env fix

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  serviceUrl: 'https://anyo-api.intellikast.in/',  
-  serviceUrl2: 'https://anyo-auth-api.intellikast.in/',
+  serviceUrl: 'https://anyo-api.intellikast.in',  
+  serviceUrl2: 'https://anyo-auth-api.intellikast.in',
   app:"admin-portal",
-  authPageUrl: 'https://anyo-auth.intellikast.in/'
+  authPageUrl: 'https://anyo-auth.intellikast.in'
 
 
 
