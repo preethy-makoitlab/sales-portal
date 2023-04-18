24224cf9381f7ab356a9d9b811a928aaaf9090fe
dev-saravana <dev@veract>
2023-04-18 17:28:23 +0530

    tailwind config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["./src/**/*.{html,ts,tsx,jsx}"],
+  content: ["./src/**/*.{html,ts}"],
   rippleui: {
 		defaultStyle: false,
 	},
