38596f8a5d2819af07c33d9fbf9430c60df1b7ad
Dev Veract <dev@veractconsultancy.in>
2023-04-27 20:21:26 +0530

    environment change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@ export const environment = {
   contentServiceUrl: 'https://anyo-files.intellikast.in',
   // contentServiceUrl: 'http://localhost:3005',
 
-  // serviceUrl: 'https://anyo-api.intellikast.in',
+  serviceUrl: 'https://anyo-api.intellikast.in',
   serviceUrl2: 'https://anyo-auth-api.intellikast.in',
-  serviceUrl: 'http://localhost:3001/',  
+  // serviceUrl: 'http://localhost:3001/',  
   // serviceUrl2: 'http://localhost:3000/',
   app: "admin-portal",
   authPageUrl: 'https://anyo-auth.intellikast.in/',
