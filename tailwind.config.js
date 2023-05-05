fc01892b37abd22dbc6e02836c60f7b874620cdc
dev-saravana <dev@veract>
2023-05-05 14:04:33 +0530

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,52 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+// module.exports = {
+//   content: ["./src/**/*.{html,ts,js}",
+//     './pages/**/*.{html,ts,js}',
+//     './components/**/*.{html,ts,js}', './*.{html,js,ts,jsx}'],
+//   rippleui: {
+//     defaultStyle: false,
+//   },
+//   theme: {
+//     extend: {
+//       // fontFamily: {
+//       //   "font-poppins": ['Poppins']
+//       // }
+//     },
+//     container: {
+//       center: true,
+//     }
+//   },
+//   plugins: [require("rippleui")]
+// }
+// // plugins: [require("rippleui")]
+
 module.exports = {
-  content: ["./src/**/*.{html,ts,js}",
-    './pages/**/*.{html,ts,js}',
-    './components/**/*.{html,ts,js}', './*.{html,js,ts,jsx}'],
+  prefix: 'tw-',
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.{html,ts}']
+  },
+  darkMode: false, // or 'media' or 'class'
   rippleui: {
     defaultStyle: false,
   },
   theme: {
     extend: {
-      // fontFamily: {
-      //   "font-poppins": ['Poppins']
-      // }
-    },
-    container: {
-      center: true,
+      fontFamily: {
+        "font-poppins": ['Poppins']
+      }
     }
   },
-  plugins: [require("rippleui")]
-}
-// plugins: [require("rippleui")]
+  variants: {
+    extend: {}
+  },
+  plugins: [
+    require("rippleui")({
+      flat: true,
+      squared: true,
+      disabled: true
+    })
+  ]
+};
