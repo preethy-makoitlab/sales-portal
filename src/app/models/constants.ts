680597f03d26e1feef7c4f0febec07e5fa6b0024
Dev Veract <dev@veractconsultancy.in>
2023-06-13 11:24:18 +0530

    readme updated

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -1,15 +1,5 @@
 import { Language } from './common-models';
 
-export const LANGUAGES: Language[] = [{ name: 'English', code: 'en-IN' }, { name: 'தமிழ்', code: 'ta-IN' }, { name: 'हिन्दी', code: 'hi-IN' }]
-
-export enum ApprovalStatus {
-    NEW = 1,
-    APPROVED = 2,
-    INPROGRESS = 3,
-    REJECTED = 4,
-    ONHOLD = 5
-}
-
 export enum ApiType{
     get = 'get',
     post = 'post',
